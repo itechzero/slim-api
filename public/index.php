fa1d651fea08d8a62dc81d3c07c33a13c37abe5d
Vortex <xvscode@gmail.com>
2021-01-17 21:50:29 +0800

    commit message

--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+if (true) { // Should be set to true in production
+    $containerBuilder->enableCompilation(__DIR__ . '/../var/cache');
+}
+// Set up settings
+$settings = require __DIR__ . '/../config/settings.php';
+$settings($containerBuilder);
 
 $app = AppFactory::create();
 
